2686282c78068f3b4116cfe0e851bcf7be3d6ffb
DevLeonid <devleonid@gmail.com>
2019-08-26 14:24:06 +0800

    Adding setting changes

--- src/3mw_backend_service/app/wsgi.py
+++ src/3mw_backend_service/app/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '3mw_backend_service.app.settings')
 
 application = get_wsgi_application()
